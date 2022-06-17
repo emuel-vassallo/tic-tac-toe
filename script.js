92b0a1eb780464cf797e1369751a858b4ca40ebe
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-17 20:05:05 +0200

    add function that adds mark to board

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
-const GameBoard = ((doc) => {
-  const gameBoard = [
-    ['X', 'O', 'O'],
-    ['O', 'X', 'O'],
-    ['X', 'O', 'X'],
-  ];
+const Gameboard = (() => {
+  const gameboard = ['', '', '', '', '', '', '', '', ''];
 
-  const renderBoard = (selector) => {
-    for (let i = 0; i < gameBoard[0].length; i++) {
-      for (let j = 0; j < gameBoard.length; j++) {
-        const symbol = gameBoard[i][j];
-        const boardSymbolElement = document.createElement('p');
-        boardSymbolElement.textContent = symbol;
-        boardSymbolElement.classList.add('board-symbol');
-        doc.querySelector(selector).append(boardSymbolElement);
-      }
+  const renderBoard = (gameboardSelector) => {
+    document.querySelector(gameboardSelector).innerHTML = '';
+
+    for (let i = 0; i < gameboard.length; i++) {
+      const boardMarkElement = document.createElement('button');
+
+      boardMarkElement.classList.add('board-mark-button');
+      boardMarkElement.setAttribute('data-board-mark-number', i);
+      boardMarkElement.textContent = gameboard[i];
+
+      document.querySelector(gameboardSelector).append(boardMarkElement);
     }
   };
 
-  return {
-    renderBoard,
-  };
-})(document);
+  return { gameboard, renderBoard };
+})();
 
-GameBoard.renderBoard('.game-board');
+const Player = (mark) => {
+  const getMark = () => mark;
+
+  const addMarkToBoard = (symbol, positionNumber) => {
+    const isMarkOccupied = Gameboard.gameboard[positionNumber];
+    if (positionNumber < 0 || positionNumber > 8 || isMarkOccupied) return;
+    Gameboard.gameboard[positionNumber] = symbol;
+    Gameboard.renderBoard('.game-board');
+  };
+  
+  return {
+    getMark,
+    addMarkToBoard,
+  };
+};
+
+Gameboard.renderBoard('.game-board');
+
+const player1 = Player('X');
+const player2 = Player('O');
