de6ac9cd2ecc547cab58d202f49e547499020122
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-18 16:36:27 +0200

    refactor js code

--- script.js
+++ script.js
@@ -4,89 +4,73 @@ const Gameboard = (() => {
   const renderBoard = (gameboardSelector) => {
     for (let i = 0; i < gameboard.length; i++) {
       const boardMarkElement = document.createElement('button');
-
       boardMarkElement.classList.add('board-mark-button');
       boardMarkElement.setAttribute('data-board-mark-number', i);
-      boardMarkElement.textContent = gameboard[i];
-
       document.querySelector(gameboardSelector).append(boardMarkElement);
     }
   };
 
-  const checkArrayEqualElements = (array) => {
-    if (typeof array !== 'undefined')
-      return !!array.reduce((a, b) => (a === b ? a : NaN));
-    return 'Array is Undefined';
-  };
+  const areArrayElementsEqual = (array) =>
+    !!array.reduce((a, b) => (a === b ? a : NaN));
 
   const isGameOver = () => {
     const isRowWin =
-      checkArrayEqualElements(gameboard.slice(0, 3)) ||
-      checkArrayEqualElements(gameboard.slice(3, 6)) ||
-      checkArrayEqualElements(gameboard.slice(6, 9));
+      areArrayElementsEqual(gameboard.slice(0, 3)) ||
+      areArrayElementsEqual(gameboard.slice(3, 6)) ||
+      areArrayElementsEqual(gameboard.slice(6, 9));
 
     const isColumnWin =
-      checkArrayEqualElements([gameboard[0], gameboard[3], gameboard[6]]) ||
-      checkArrayEqualElements([gameboard[1], gameboard[4], gameboard[7]]) ||
-      checkArrayEqualElements([gameboard[2], gameboard[5], gameboard[8]]);
+      areArrayElementsEqual([gameboard[0], gameboard[3], gameboard[6]]) ||
+      areArrayElementsEqual([gameboard[1], gameboard[4], gameboard[7]]) ||
+      areArrayElementsEqual([gameboard[2], gameboard[5], gameboard[8]]);
 
     const isDiagonalWin =
-      checkArrayEqualElements([gameboard[0], gameboard[4], gameboard[8]]) ||
-      checkArrayEqualElements([gameboard[2], gameboard[4], gameboard[6]]);
+      areArrayElementsEqual([gameboard[0], gameboard[4], gameboard[8]]) ||
+      areArrayElementsEqual([gameboard[2], gameboard[4], gameboard[6]]);
 
     const isTie = gameboard.every((e) => e);
 
     return isRowWin || isColumnWin || isDiagonalWin || isTie;
   };
 
-  const addMarkOnClick = (player1Mark, player2Mark) => {
-    const boardButtons = document.querySelectorAll('.board-mark-button');
-    let hasPlayer1Clicked = false;
-
-    for (const button of boardButtons) {
-      button.addEventListener('click', () => {
-        const boardMarkNumberClicked = button.dataset.boardMarkNumber;
-        const isMarkOccupied = Gameboard.gameboard[boardMarkNumberClicked];
-
-        if (
-          boardMarkNumberClicked < 0 ||
-          boardMarkNumberClicked > 8 ||
-          isMarkOccupied
-        )
-          return;
-
-        const gameboardButton = boardButtons[boardMarkNumberClicked];
-
-        if (hasPlayer1Clicked) {
-          gameboard[boardMarkNumberClicked] = player1Mark;
-          gameboardButton.textContent = player1Mark;
-          hasPlayer1Clicked = false;
-          if (isGameOver()) console.log('Game over!');
-          return;
-        }
-
-        gameboard[boardMarkNumberClicked] = player2Mark;
-        gameboardButton.textContent = player2Mark;
-        hasPlayer1Clicked = true;
-        if (isGameOver()) console.log('Game over!');
-      });
-    }
-  };
-
-  return { gameboard, renderBoard, addMarkOnClick };
+  return { gameboard, renderBoard, isGameOver };
 })();
 
 const Player = (mark) => {
   const getMark = () => mark;
-
   return {
     getMark,
   };
 };
 
-Gameboard.renderBoard('.game-board');
+const displayController = (() => {
+  const addMarkOnClick = (player1Mark, player2Mark) => {
+    let playerMarksIndex = 0;
+    let playerMarks = [player1Mark, player2Mark];
+    const boardButtons = document.querySelectorAll('.board-mark-button');
 
-const player1 = Player('X');
-const player2 = Player('O');
+    for (const button of boardButtons) {
+      button.addEventListener('click', () => {
+        const buttonNumClicked = button.dataset.boardMarkNumber;
+        if (Gameboard.gameboard[buttonNumClicked]) return;
+        Gameboard.gameboard[buttonNumClicked] = playerMarks[playerMarksIndex];
+        boardButtons[buttonNumClicked].textContent = playerMarks[0];
+        if (Gameboard.isGameOver()) console.log('Game over!');
+        playerMarks = playerMarks.reverse();
+      });
+    }
+  };
+  return { addMarkOnClick };
+})();
 
-Gameboard.addMarkOnClick(player1.getMark(), player2.getMark());
+const flowController = (() => {
+  const main = () => {
+    Gameboard.renderBoard('.game-board');
+    const player1 = Player('X');
+    const player2 = Player('O');
+    displayController.addMarkOnClick(player1.getMark(), player2.getMark());
+  };
+  return { main };
+})();
+
+flowController.main();
