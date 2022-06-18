65d66962f9d34080f6d76bc9322204e44360d9ba
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-18 11:47:02 +0200

    check if game is over

--- script.js
+++ script.js
@@ -13,6 +13,32 @@ const Gameboard = (() => {
     }
   };
 
+  const checkArrayEqualElements = (array) => {
+    if (typeof array !== 'undefined')
+      return !!array.reduce((a, b) => (a === b ? a : NaN));
+    return 'Array is Undefined';
+  };
+
+  const isGameOver = () => {
+    const isRowWin =
+      checkArrayEqualElements(gameboard.slice(0, 3)) ||
+      checkArrayEqualElements(gameboard.slice(3, 6)) ||
+      checkArrayEqualElements(gameboard.slice(6, 9));
+
+    const isColumnWin =
+      checkArrayEqualElements([gameboard[0], gameboard[3], gameboard[6]]) ||
+      checkArrayEqualElements([gameboard[1], gameboard[4], gameboard[7]]) ||
+      checkArrayEqualElements([gameboard[2], gameboard[5], gameboard[8]]);
+
+    const isDiagonalWin =
+      checkArrayEqualElements([gameboard[0], gameboard[4], gameboard[8]]) ||
+      checkArrayEqualElements([gameboard[2], gameboard[4], gameboard[6]]);
+
+    const isTie = gameboard.every((e) => e);
+
+    return isRowWin || isColumnWin || isDiagonalWin || isTie;
+  };
+
   const addMarkOnClick = (player1Mark, player2Mark) => {
     const boardButtons = document.querySelectorAll('.board-mark-button');
     let hasPlayer1Clicked = false;
@@ -35,12 +61,14 @@ const Gameboard = (() => {
           gameboard[boardMarkNumberClicked] = player1Mark;
           gameboardButton.textContent = player1Mark;
           hasPlayer1Clicked = false;
+          if (isGameOver()) console.log('Game over!');
           return;
         }
 
         gameboard[boardMarkNumberClicked] = player2Mark;
         gameboardButton.textContent = player2Mark;
         hasPlayer1Clicked = true;
+        if (isGameOver()) console.log('Game over!');
       });
     }
   };
@@ -62,4 +90,3 @@ const player1 = Player('X');
 const player2 = Player('O');
 
 Gameboard.addMarkOnClick(player1.getMark(), player2.getMark());
-// player2.updateBoardOnClick();
