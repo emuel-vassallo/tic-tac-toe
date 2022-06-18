913e2035d9ae069e8df99fac057170e33d48dacd
emuel-vassallo <emuelvassallo15@gmail.com>
2022-06-18 17:57:03 +0200

    highlight winning board cells

--- script.js
+++ script.js
@@ -4,36 +4,53 @@ const Gameboard = (() => {
   const renderBoard = (gameboardSelector) => {
     for (let i = 0; i < gameboard.length; i++) {
       const boardMarkElement = document.createElement('button');
-      boardMarkElement.classList.add('board-mark-button');
+
+      boardMarkElement.classList.add('board-button');
       boardMarkElement.setAttribute('data-board-mark-number', i);
+
       document.querySelector(gameboardSelector).append(boardMarkElement);
     }
+  };
+
+  const disableGameboard = () => {
+    const boardButtons = document.querySelectorAll('.board-button');
+    boardButtons.forEach((button) => (button.disabled = true));
   };
 
   const areArrayElementsEqual = (array) =>
     !!array.reduce((a, b) => (a === b ? a : NaN));
 
-  const isGameOver = () => {
-    const isRowWin =
-      areArrayElementsEqual(gameboard.slice(0, 3)) ||
-      areArrayElementsEqual(gameboard.slice(3, 6)) ||
-      areArrayElementsEqual(gameboard.slice(6, 9));
+  const isTie = gameboard.every((e) => e);
 
-    const isColumnWin =
-      areArrayElementsEqual([gameboard[0], gameboard[3], gameboard[6]]) ||
-      areArrayElementsEqual([gameboard[1], gameboard[4], gameboard[7]]) ||
-      areArrayElementsEqual([gameboard[2], gameboard[5], gameboard[8]]);
+  const getWinningBoardCells = () => {
+    if (areArrayElementsEqual(gameboard.slice(0, 3))) return [0, 1, 2];
+    if (areArrayElementsEqual(gameboard.slice(3, 6))) return [3, 4, 5];
+    if (areArrayElementsEqual(gameboard.slice(6, 9))) return [6, 7, 8];
 
-    const isDiagonalWin =
-      areArrayElementsEqual([gameboard[0], gameboard[4], gameboard[8]]) ||
-      areArrayElementsEqual([gameboard[2], gameboard[4], gameboard[6]]);
+    if (areArrayElementsEqual([gameboard[0], gameboard[3], gameboard[6]]))
+      return [0, 3, 6];
+    if (areArrayElementsEqual([gameboard[1], gameboard[4], gameboard[7]]))
+      return [1, 4, 7];
+    if (areArrayElementsEqual([gameboard[2], gameboard[5], gameboard[8]]))
+      return [2, 5, 8];
 
-    const isTie = gameboard.every((e) => e);
+    if (areArrayElementsEqual([gameboard[0], gameboard[4], gameboard[8]]))
+      return [0, 4, 8];
+    if (areArrayElementsEqual([gameboard[2], gameboard[4], gameboard[6]]))
+      return [2, 4, 6];
 
-    return isRowWin || isColumnWin || isDiagonalWin || isTie;
+    return [];
   };
 
-  return { gameboard, renderBoard, isGameOver };
+  const isGameOver = () => isTie || getWinningBoardCells().length > 0;
+
+  return {
+    gameboard,
+    renderBoard,
+    isGameOver,
+    getWinningBoardCells,
+    disableGameboard,
+  };
 })();
 
 const Player = (mark) => {
@@ -44,10 +61,22 @@ const Player = (mark) => {
 };
 
 const displayController = (() => {
+  const addHighlightClass = () => {
+    const boardButtons = document.querySelectorAll('.board-button');
+    const winningBoardCells = Gameboard.getWinningBoardCells();
+    let winningBoardButtons = [];
+    for (const cell of winningBoardCells) {
+      winningBoardButtons = [...winningBoardButtons, boardButtons[cell]];
+    }
+    for (button of winningBoardButtons) {
+      button.classList.add('win-highlight');
+    }
+  };
+
   const addMarkOnClick = (player1Mark, player2Mark) => {
     let playerMarksIndex = 0;
     let playerMarks = [player1Mark, player2Mark];
-    const boardButtons = document.querySelectorAll('.board-mark-button');
+    const boardButtons = document.querySelectorAll('.board-button');
 
     for (const button of boardButtons) {
       button.addEventListener('click', () => {
@@ -55,11 +84,15 @@ const displayController = (() => {
         if (Gameboard.gameboard[buttonNumClicked]) return;
         Gameboard.gameboard[buttonNumClicked] = playerMarks[playerMarksIndex];
         boardButtons[buttonNumClicked].textContent = playerMarks[0];
-        if (Gameboard.isGameOver()) console.log('Game over!');
+        if (Gameboard.isGameOver()) {
+          addHighlightClass();
+          Gameboard.disableGameboard();
+        }
         playerMarks = playerMarks.reverse();
       });
     }
   };
+
   return { addMarkOnClick };
 })();
 
@@ -70,6 +103,7 @@ const flowController = (() => {
     const player2 = Player('O');
     displayController.addMarkOnClick(player1.getMark(), player2.getMark());
   };
+
   return { main };
 })();
 
